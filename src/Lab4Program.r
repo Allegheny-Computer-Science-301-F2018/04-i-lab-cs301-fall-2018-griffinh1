4bc3b230b675561c4bd9d84778a4c73c0b1bdaa5
Haeley Griffin <haeleygriffin@Haeleys-MacBook-Pro.local>
2018-10-12 00:11:12 -0400

    Still need to finish 7&9

--- src/Lab4Program.r
+++ src/Lab4Program.r
@@ -4,22 +4,48 @@
 # Run the below only if the library is not already installed.
 # install.packages(dslabs)
 
-library(dslabs)
-library(dplyr)
-library(tidyverse)
-data(us_contagious_diseases)
+ library(dslabs)
+ library(dplyr)
+ library(tidyverse)
+ data(us_contagious_diseases)
 
 #Question 1.
-
+ 
+ dat <- filter(us_contagious_diseases, disease == "Measles", state != "Alaska", state != "Hawaii")
+ dat <- mutate(dat, per100000rate = (((count*100000)/population)*(weeks_reporting/52)))
+ 
 #Question 2.
-
+ 
+ dat <- filter(dat, disease == "Measles", state == "California")
+ ggplot(data = dat, mapping = aes(x = year, y = per100000rate)) + geom_vline(xintercept = 1965) + geom_line()
+ 
 #Question 3.
-
+ 
+ dat_caliFocus$yearBlock[dat_caliFocus$year >= 1950 & dat_caliFocus$year <= 1959] <- "1950’s"
+ dat_caliFocus$yearBlock[dat_caliFocus$year >= 1960 & dat_caliFocus$year <= 1969] <- "1960’s"
+ dat_caliFocus$yearBlock[dat_caliFocus$year >= 1970 & dat_caliFocus$year <= 1979] <- "1970’s"
+ 
+ ggplot(data = dat_caliFocus) + geom_histogram(mapping = aes(x = year,  y = count, fill = yearBlock), position = "dodge", stat ="identity")
+ ggplot(data = dat_caliFocus) + geom_histogram(mapping = aes(x = year,  y = sqrt(count), fill = yearBlock), position = "dodge", stat= "identity")
+ 
 #Question 4.
 
+ dat_USFocus$yearBlock[dat_USocus$year >= 1950 & dat_USFocus$year <= 1959] <- "1950’s"
+ dat_USiFocus$yearBlock[dat_USFocus$year >= 1960 & dat_USFocus$year <= 1969] <- "1960’s"
+ dat_USFocus$yearBlock[dat_USFocus$year >= 1970 & dat_USFocus$year <= 1979] <- "1970’s"
+ ggplot(data = dat_USFocus) + geom_histogram(mapping = aes(x = year,  y = sqrt(count), fill = yearBlock), position = "dodge", stat= "identity")
+ ggplot(data = dat_USFocus) + geom_histogram(mapping = aes(x = year,  y = count, fill = yearBlock), position = "dodge", stat= "identity")
+ 
 #Question 5.
-
+ 
+ dat <- mutate(dat_USFocus, per100000rate = (((count*100000)/population)*(weeks_reporting/52)))
+ ggplot(data = dat, mapping = aes(x=year, y=state)) + geom_tile(mapping = aes(fill = per100000rate))
+ 
 #Question 6.
+ 
+ #https://www.cdc.gov/ncbddd/autism/documents/ASDPrevalenceDataTable2016.pdf
+ #In this report of data I found could not be plotted but I did see that the number for autism did increase. 
+ 
 
 
 
--- writing/reportAndReflections.md
+++ writing/reportAndReflections.md
@@ -6,9 +6,22 @@ Question 7.
 
 
 Question 8.
+Q:What is the damage to the public medicine and public opinion from such an article which states (incorrectly) that autism is a result of vaccines?
+
+A:The damage is that with this article released many parents will take it as the truth and not get their kids vaccinated and it will increase the virus rate because more children will be prone to getting it because they will not have been vaccinated.
+
+Q:What should the role of academic research groups and organizations be to ensure that published information is absolutely correct (i.e., has been properly analyzed) before public exposure?
+
+A:The academic researchers should have to go though thorough examinations of all of the information before it it published to ensure that the information is credible and correct. We do not need anymore incorrect information to be released to the public.
+
+Q:The researcher retracted his paper, which means that he no longer supports its content. Is retracting a paper enough to fix the damage to public medicine? How could the damage be fixed if this you do not think that this is enough?
+
+A:No I do not believe retracting a paper is enough. I believe the man went to jail and I think that is also something that should happen. He put many children's lives at risk. People doing research should not only be looking for correctness they need to look at the ethics of their studies as well.
+
 
 
 Question 9.
+
 
 
 Question 10.
